144393f9fc2f8108ccc4d7e4ad3e1c8e7bde5224
przygoda <schmidt.multimedia@googlemail.com>
2023-02-14 08:32:10 +0000

    user-Tabelle / CREATE

---
+++ 03_user_DB/03a_db_userDB_SQL.sql
@@ -0,0 +1,14 @@
+/* USER-Tabelle  Version 1 */
+
+DROP TABLE IF EXISTS boo.users;
+
+CREATE TABLE IF NOT EXISTS boo.users
+(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	userName VARCHAR(20) NOT NULL UNIQUE,
+	familyName VARCHAR(20) NOT NULL,
+	firstName VARCHAR(20) NOT NULL
+);
+
+/* Struktur */
+DESCRIBE boo.users;
